d22e7616fbc01f314220002d994d4e25e415582a
lunaticsy <lunatic.yi@gmail.com>
2024-03-12 14:01:03 +0900

    typo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,10 +28,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "0592dfe3b97f6e3fd0019861b69d1ee6",
 "icons/Icon-maskable-192.png": "8a4cfb33d7601f7799771549f89ad5f1",
 "icons/Icon-maskable-512.png": "0592dfe3b97f6e3fd0019861b69d1ee6",
-"index.html": "96c8d13f1d562eede023b91b3b5993cc",
-"/": "96c8d13f1d562eede023b91b3b5993cc",
+"index.html": "11de2194c3b48511989b3ec73614b926",
+"/": "11de2194c3b48511989b3ec73614b926",
 "loading.css": "86be72326e14b11bf4ad6eac92740ae8",
-"main.dart.js": "70cac71c136f5590126e33ee61f67961",
+"main.dart.js": "25ee835979ef59bb0f1d983c5b7bda05",
 "manifest.json": "af1c1026f4bcac4e352c1835b200e232",
 "version.json": "b128068ddefb3b31e503f20399c01174"
 };
